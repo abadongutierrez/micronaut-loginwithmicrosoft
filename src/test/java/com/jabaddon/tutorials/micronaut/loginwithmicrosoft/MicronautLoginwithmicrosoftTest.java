b09e75fe09a51b0c913f70e591e971484b5a84f1
rgutierrez <rgutierrez@switchfly.com>
2021-01-26 23:12:28 -0600

    Starting the project

---
+++ src/test/java/com/jabaddon/tutorials/micronaut/loginwithmicrosoft/MicronautLoginwithmicrosoftTest.java
@@ -0,0 +1,21 @@
+package com.jabaddon.tutorials.micronaut.loginwithmicrosoft;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class MicronautLoginwithmicrosoftTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
